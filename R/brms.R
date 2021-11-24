7ac7861dbe76df0893636122e1ffb6e6f6d80d43
catarinawor <catarinawor@gmail.com>
2021-11-23 16:01:27 -0800

    more attempts at the bmrs

--- R/brms.R
+++ R/brms.R
@@ -59,7 +59,6 @@ irecall<-irecall%>%left_join(arealu[,c("AREA","LU_GROUPING3")])
 
 
 irecc<- irecall %>%
-  #rename(IREC=ESTIMATE)%>%
   select(c(AREA,YEAR,MONTH,DISPOSITION,ESTIMATE,VARIANCE,LU_GROUPING3))%>%
   group_by( AREA,YEAR,MONTH,DISPOSITION,LU_GROUPING3) %>% summarise(ESTIMATE = sum(ESTIMATE), VARIANCE = sum(VARIANCE))%>%
   mutate(SD=sqrt(VARIANCE))%>%
@@ -87,23 +86,36 @@ rename(IREC=ESTIMATE,SDIREC=SD)%>%
 datxy<-left_join(ireccc,creelcc) %>%
 mutate(SEASON=if_else(MONTH<5|MONTH>9,"offseason","peakseason"))
 
+summary(datxy)
+
+datxy$diff <- datxy$CREEL - datxy$IREC
+p <- ggplot(datxy,aes(y=diff, x=as.factor(MONTH)))
+p <- p + geom_boxplot(width=1, position = position_dodge(width = 1))
+p <- p +geom_hline(aes(yintercept=0),size=1.1, alpha=.5)
+p <- p + scale_color_viridis_d(end = 0.8,option = "A")
+p <- p + theme_bw(16)+labs( x="month", y="creel-irec")
+
+p
+
 #===========================
+#histograms by groupings - of the data that is complete
+datnna<-datxy[!is.na(datxy$CREEL),]
+
+
 #proportion of 0's in iredc
 
 prop0<-function(x){sum(x==0)/length(x)}
 
-summary(datxy)
 
-aggregate(datxy$IREC,by=list(datxy$MONTH),prop0)
-aggregate(datxy$IREC,by=list(datxy$LU_GROUPING3),prop0)
-
-aggregate(datxy$IREC,by=list(datxy$YEAR),prop0)
+aggregate(datnna$CREEL,by=list(datnna$MONTH),prop0)
+aggregate(datnna$CREEL,by=list(datnna$LU_GROUPING3),prop0)
+aggregate(datnna$CREEL,by=list(datnna$YEAR),prop0)
 
 
 
 #===========================================
 #plot data by area
-p <- ggplot(datxy,aes(x=CREEL, y=IREC,color=LU_GROUPING3))
+p <- ggplot(datnna,aes(y=CREEL, x=IREC,color=LU_GROUPING3))
 p <- p + geom_point(size=2, alpha=.5)
 p <- p + geom_smooth(method = lm, formula= y~0+x, se     = FALSE, size   = 1, alpha  = .8) # to add regression line
 p <- p + theme_bw(16)+labs( x="CREEL", y="iREC")
@@ -111,12 +123,28 @@ p <- p + scale_color_viridis_d(end = 0.8,option = "C")
 p <- p + theme(legend.position="bottom")
 p
 
+#plot data by month
+p <- ggplot(datnna,aes(y=CREEL, x=IREC,color=as.factor(MONTH)))
+p <- p + geom_point(size=2, alpha=.5)
+p <- p + geom_smooth(method = lm, formula= y~0+x, se     = FALSE, size   = 1, alpha  = .8) # to add regression line
+p <- p + theme_bw(16)+labs( x="CREEL", y="iREC")
+p <- p + scale_color_viridis_d(end = 0.8,option = "B")
+p <- p + theme(legend.position="bottom")
+p
 
+#plot data by year
+p <- ggplot(datnna,aes(y=CREEL, x=IREC,color=as.factor(YEAR)))
+p <- p + geom_point(size=2, alpha=.5)
+p <- p + geom_smooth(method = lm, formula= y~0+x, se     = FALSE, size   = 1, alpha  = .8) # to add regression line
+p <- p + theme_bw(16)+labs( x="CREEL", y="iREC")
+p <- p + scale_color_viridis_d(end = 0.8,option = "B")
+p <- p + theme(legend.position="bottom")
+p
 #=============================
-#histograms by groupings - of the data that is complete
-datnna<-datxy[!is.na(datxy$CREEL),]
 
-p <- ggplot(datnna,aes(IREC))
+
+
+p <- ggplot(datnna,aes(CREEL))
 p <- p + geom_histogram()
 p <- p + facet_wrap(~LU_GROUPING3, scales="free")
 p <- p + theme_bw(16)
@@ -143,14 +171,36 @@ aggregate(dats$MONTH,by=list(dats$MONTH),length)
 #Also: distribution? What should I consider? Lognormal (without 0s) an dhurdle lognormal are the 
 #only ones that seem to converge, but diagnostics do not look good, data seems to be overdispersed (hurdle).
 # and not fitting higher values with the positive only model.
-fit2 <- brm(formula= IREC  ~ -1 + s(MONTH, k=3)+ (-1 + CREEL  |LU_GROUPING3) , 
-  data=dats, family=hurdle_lognormal, iter = 800,chains=2 )
+#fit2 <- brm(formula= IREC  ~ -1 + s(MONTH, k=3) + (-1 + CREEL  |LU_GROUPING3) , 
+#  data=dats, family=hurdle_lognormal, iter = 800,chains=2 )
 
 
+#flip the model 
+#if the s(MONTH, k=3) term is included then loo() crashes R
+fit2 <- brm(formula=  CREEL ~ -1 +  IREC + (-1 +  IREC |LU_GROUPING3) , 
+  data=dats, family=hurdle_lognormal, iter = 1000,chains=2 )
+
+dim(dats)
 
 summary(fit2)
 pairs(fit2)
 plot(fit2, ask = FALSE)
+
+#I do not undertand what is causing the extreme outliers in the predicted values
+
+fitted_values <- fitted(fit2)
+head(fitted_values)
+dat <- as.data.frame(cbind(Y = standata(fit2)$Y, fitted_values))
+ggplot(dat) + geom_point(aes(x = Estimate, y = Y))
+
+
+
+#the problematic data 
+dats[which.max(fitted_values[,1]),]
+
+
+
+#this plot makes no sense to me:
 conditional_effects(fit2, method="posterior_predict")
 
 
